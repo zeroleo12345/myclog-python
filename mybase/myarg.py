5416234a57ebb7943ac2a2d7164c0ff4ec17babc
zeroleo12345 <zeroleo12345@163.com>
2019-06-30 14:53:26 +0800

    add comment

--- mybase/myarg.py
+++ mybase/myarg.py
@@ -6,6 +6,7 @@
         optparse: 
             optparse was officially deprecated after Python 2.7
 """
+from __future__  import print_function
 import sys
 
 
@@ -14,9 +15,7 @@ import sys
 def init_args():
     import argparse
     parser = argparse.ArgumentParser(description='%s' % (sys.argv[0]))
-    parser.add_argument('-c', metavar='<FILE>', type=str, help='config file path', required=False, dest='conf')
-    parser.add_argument('-log', metavar='<FILE>', type=str, help='log file path', required=False, dest='logfile')
-    parser.add_argument('-level', metavar='<LEVEL>', type=str, help='debug|info|warn|error', default='debug', dest="loglevel")
+    parser.add_argument('--topic', action='append', metavar='<FILE>', type=str, help='radius dictionary file', required=True)
     # 指定参数-h的回调函数: parser.print_help = Usage  # Usage为函数对象
     return parser.parse_args()
 
@@ -27,10 +26,10 @@ def init_args():
     #parser.add_argument('-p', '--port', metavar='<PORT>, type=int, help='http server port listen, default: 80')
     """ 参数: 单个字符串. -c myconf.conf   """
     #parser.add_argument('-c', metavar='<FILE>', type=str, help='config file path', required=False, dest='conf')
-    """ 参数: 多个字符串. -dict /dir/dict1 -dict /dir/dict2 """ # nargs 用于控制参数后跟几个参数
-    #parser.add_argument('-dict', action='append', metavar='<FILE>', type=str, nargs=1, help='radius dictionary file', required=True)
+    """ 参数: 多个字符串. -dict /dir/dict1 -dict /dir/dict2 """ # nargs=1 指定参数后跟几个参数, 可不使用此参数
+    #parser.add_argument('-dict', action='append', metavar='<FILE>', type=str, help='radius dictionary file', required=True)
 
 
 if __name__ == '__main__':
     args = init_args()
-    print args
+    print(args)
