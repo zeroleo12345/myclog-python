42e60eda56e3fd8d991ffda8706f253d09331c85
zeroleo12345 <zeroleo12345@163.com>
2019-10-31 20:51:17 +0800

    m

--- mybase3/myhttp.py
+++ mybase3/myhttp.py
@@ -1,4 +1,11 @@
 from urllib.parse import quote
+"""
+# 方法1:
+    urllib.parse.quote('#company#')
+
+# 方法2:
+    urllib.parse.urlencode({'#company#':'Yunpian','#code#':'1234'})
+"""
 
 def url_encode(url):
     return quote(url)
