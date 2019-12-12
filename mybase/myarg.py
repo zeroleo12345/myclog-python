e74dbdf8727433ffc672e6ef0791f38ea1904d2e
zeroleo12345 <zeroleo12345@163.com>
2019-12-12 17:59:14 +0800

    m

--- mybase/myarg.py
+++ mybase/myarg.py
@@ -24,15 +24,22 @@ def init_args():
     # 指定参数-h的回调函数: parser.print_help = Usage  # Usage为函数对象
     return parser.parse_args()
 
-    # Example:
-    """ 参数: True or False. -d                     """
-    #parser.add_argument('-d', action='store_true', help='turn on debug flag', default=False, dest="isDebug")
-    """ 参数: 数字. -p 6379                """
-    #parser.add_argument('-p', '--port', metavar='<PORT>, type=int, help='http server port listen, default: 80')
-    """ 参数: 单个字符串. -c myconf.conf   """
-    #parser.add_argument('-c', metavar='<FILE>', type=str, help='config file path', required=False, dest='conf')
-    """ 参数: 多个字符串. -dict /dir/dict1 -dict /dir/dict2 """ # nargs=1 指定参数后跟几个参数, 可不使用此参数
-    #parser.add_argument('-dict', action='append', metavar='<FILE>', type=str, help='radius dictionary file', required=True)
+    """
+    参数: True or False. -d
+        parser.add_argument('-d', action='store_true', help='turn on debug flag', default=False, dest="isDebug")
+
+    参数: 数字. -p 6379
+        parser.add_argument('-p', '--port', metavar='<PORT>, type=int, help='http server port listen, default: 80')
+
+    参数: 单个字符串. -c myconf.conf
+        parser.add_argument('-c', metavar='<FILE>', type=str, help='config file path', required=False, dest='conf')
+
+    参数: 选择. -c rock
+        parser.add_argument('-c', metavar='<FILE>', type=str, choices=['rock', 'paper'], dest='choice')
+
+    参数: 多个字符串. -dict /dir/dict1 -dict /dir/dict2  # nargs=1 指定参数后跟几个参数, 可不使用此参数
+        parser.add_argument('-dict', action='append', metavar='<FILE>', type=str, help='radius dictionary file', required=True)
+    """
 
 
 if __name__ == '__main__':
